6597e8279e9eea5fbc94f93f2c2d641d29086a30
mikelesnr <mwanza.n.m@gmail.com>
2024-06-10 16:32:52 +0200

    loops prep challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Game();
+
+        Console.Write("Do you want to play again y/n? ");    
+        string replay = Console.ReadLine().ToLower();    
+        while (replay == "y")
+        {
+            Game();
+            Console.Write("Do you want to play again y/n? ");    
+            replay = Console.ReadLine().ToLower(); 
+        }
+    }
+    static void Game()
+    {
+        Random  randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        Console.Write("Whats the magic number? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        int count = 1;
+        while (guess != number){
+            if (count > 1)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+            }
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            count++;
+        }
+        string tries = (count == 1) ? "try" : "tries";
+        Console.WriteLine($"You Guessed it in {count} {tries}!");
     }
 }
