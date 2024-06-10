3d3e21247c9a6f15d976a99cbcaf5d2f441d0bd5
mikelesnr <mwanza.n.m@gmail.com>
2024-06-10 15:40:36 +0200

    Assignment 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter grade % ");
+        int percent = int.Parse(Console.ReadLine());
+        string letter = Grade(percent);
+        string sign = Symbol(percent);
+        string grade = $"{sign}{letter}";
+        PrintGrade(grade);
+        if(percent >= 70) 
+        {
+            Console.WriteLine("You passed the class 🎊");
+        }
+        else{
+            Console.WriteLine("You'll make it next time");
+        }
+    }
+
+    static string Grade(int value)
+    {
+        if (value >= 90)
+        {
+            return "A";
+        }
+        else if (value >= 80)
+        {
+            return "B";
+        }
+        else if (value >= 70)
+        {
+            return "C";
+        }
+        else if (value >= 60)
+        {
+            return "D";
+        }
+        else
+        {
+            return "F";
+        }
+    }
+
+    static void PrintGrade(string message){
+        Console.WriteLine($"Your grade is {message}");
+    }
+
+    static string Symbol(int score)
+    {
+        int lastDigit = score % 10;
+        if(score >= 60){
+            if(score <= 90 & lastDigit >= 7){
+                return "+";
+            }
+            if(lastDigit < 3){
+                return "-";
+            }
+
+        }
+        return "";
     }
 }
+
